7bde56a6b5c0b9a63daece16efc5a7a88a70154a
riya more <you@example.com>
2023-02-23 01:09:35 +0530

    Add New Contact

--- Program.cs
+++ Program.cs
@@ -1,10 +1,34 @@
 ﻿
-namespace AddressBooks
+namespace AddressBook
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine("Welcome to the Address Book problem");
+            AddressBook addBook = new AddressBook();
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Select 1 : To Create Contact");
+                Console.WriteLine("Select 2 : To Exit Creating Contact");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("");
+                        addBook.CreateContact();
+                        addBook.Display();
+                        Console.ReadLine();
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+                }
+            }
         }
     }
-}
+}s
+
+
